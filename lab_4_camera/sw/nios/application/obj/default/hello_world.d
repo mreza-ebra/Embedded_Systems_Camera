cfeaee5b4287a076c1dded4e9e7f55479acc5316
mreza-ebra <mohammadrza.ebrahimi@gmail.com>
2021-01-04 17:14:40 +0100

    Added top level files

---
+++ lab_4_camera/sw/nios/application/obj/default/hello_world.d
@@ -0,0 +1,16 @@
+obj/default/hello_world.o: hello_world.c \
+ c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\io.h \
+ c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\alt_types.h \
+ C:/Users/craio/Documents/git/Embedded_Systems_Camera/lab_4_camera/sw/nios/camera_bsp/system.h \
+ C:/Users/craio/Documents/git/Embedded_Systems_Camera/lab_4_camera/sw/nios/camera_bsp/linker.h \
+ i2c/i2c.h
+
+c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\io.h:
+
+c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\alt_types.h:
+
+C:/Users/craio/Documents/git/Embedded_Systems_Camera/lab_4_camera/sw/nios/camera_bsp/system.h:
+
+C:/Users/craio/Documents/git/Embedded_Systems_Camera/lab_4_camera/sw/nios/camera_bsp/linker.h:
+
+i2c/i2c.h:
---
+++ lab_4_camera/sw/nios/application/obj/default/i2c.d
@@ -0,0 +1,13 @@
+obj/default/i2c.o: i2c.c i2c.h i2c_regs.h i2c_io.h \
+ c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\io.h \
+ c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\alt_types.h
+
+i2c.h:
+
+i2c_regs.h:
+
+i2c_io.h:
+
+c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\io.h:
+
+c:\users\craio\documents\git\embedded_systems_camera\lab_4_camera\sw\nios\camera_bsp\hal\inc\alt_types.h:
